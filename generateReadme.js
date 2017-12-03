8c40ae50d094041d690dbc32bc591f1f6b51b756
Francesco Cioria <firefelix@gmail.com>
2017-12-03 13:51:07 +0100

    clean: remove useless files

--- generateReadme.js
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import { generateReadme } from 'react-readme-generator';
-
-generateReadme(path.resolve('src/FlexView.js'));
--- typings/lodash.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'lodash.pick';
-declare module 'lodash.omit';
-declare module 'lodash.some';
